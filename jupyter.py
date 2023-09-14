356a5fac367c03543591519d2944811828c52148
drammen94 <danhnguyen94@hotmail.com>
2023-09-14 14:49:35 +0200

    Update jupyter.py

--- jupyter.py
+++ jupyter.py
@@ -7,14 +7,14 @@ c.IPKernelApp.pylab = 'inline'  # if you want plotting support always in your no
 
 # Notebook config
 c.NotebookApp.notebook_dir = 'nbs'
-c.NotebookApp.allow_origin = u'mast479.herokuapp.com' # put your public IP Address here
+c.NotebookApp.allow_origin = '*' # put your public IP Address here
 c.NotebookApp.ip = '*'
 c.NotebookApp.allow_remote_access = True
 c.NotebookApp.open_browser = False
 
 # ipython -c "from notebook.auth import passwd; passwd()"
 c.NotebookApp.password = u''
-c.NotebookApp.token = ''
+c.NotebookApp.token = '4a72cb6f71e0f05a6aa931a5e0ec70109099ed0c35f1d840'
 c.NotebookApp.port = int(os.environ.get("PORT", 8888))
 c.NotebookApp.allow_root = True
 c.NotebookApp.allow_password_change = True
